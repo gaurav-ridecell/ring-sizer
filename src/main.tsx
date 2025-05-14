0727777c52afab30be0cb797d17023b4a0e67178
Gaurav Burjwal <gaurav.burjwal@ridecell.com>
2025-05-14 11:49:06 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,6 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-const renderMyApp = elementId => {
-  createRoot(document.getElementById(elementId)).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-};
 
-declare global {
-  interface Window {
-    mount: (elementId: string) => void;
-  }
-}
-
-window.mount = renderMyApp;
-
-// renderMyApp('root');
+createRoot(document.getElementById("root")!).render(<App />);
